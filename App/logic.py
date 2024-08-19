ffdd9d62bd73b6b5168e4adf8986a3d755f1b700
soloteck <jj.rojasa12@uniandes.edu.co>
2024-08-19 08:43:46 -0500

    cambios est 1

--- App/logic.py
+++ App/logic.py
@@ -114,7 +114,8 @@ def add_book_tags_file(catalog, booktagsfile):
     Esta funcion guardar los booktags provenientes del archivo CSV.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    catalog["book_tags"] =set.new_set()
+    return catalog
 
 
 def create_book_tag_list(catalog):
@@ -122,7 +123,8 @@ def create_book_tag_list(catalog):
     Esta funcion crea una lista vacia para booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    catalog["book_tags"] =set.new_set()
+    return catalog
 
 
 def add_book_tag(catalog, booktag):
@@ -130,7 +132,8 @@ def add_book_tag(catalog, booktag):
     Esta funcion agrega un elemento a lista de booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    set.add_element(catalog["book_tags"], booktag)
+    return catalog
 
 
 # Funciones de consulta
