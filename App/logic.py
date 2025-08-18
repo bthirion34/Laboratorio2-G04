68c9fbdd899baa698145136fb2f35858c1b03bc3
basto34 <bastien.thirion@eleves.ec-nantes.fr>
2025-08-18 17:27:28 -0500

    cambios del Estudiante 3

--- App/logic.py
+++ App/logic.py
@@ -119,37 +119,32 @@ def load_tags(catalog, filename):
     return tag_size(catalog)
 
 
-def load_books_tags(catalog, filename):
-    """
-    Carga los tags de los libros del archivo y los agrega a la lista
-    de tags. Siga el mismo procedimiento que en la carga de libros.
-
-    :param catalog: Catalogo de la aplicación
-    :type catalog: dict
-    :param filename: Nombre del archivo csv con los tags de los libros
-    :type filename: str
-
-    :returns: Tamaño del conjunto de tags de los libros
-    :rtype: int
-    """
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+def load_books_tags(catalog, filename): 
+    """ 
+    Cargo los tags de los libros del archivo 
+    """ 
+    # TODO: Mods de Est-1 y Est-2, Est-3 en el Lab 2 
+    booktagfile = os.path.join(data_dir, filename) 
+    input_file = csv.DictReader(open(booktagfile, encoding="utf-8")) 
+    catalog = create_book_tag_list(catalog) 
+    for booktag in input_file: 
+        add_book_tag(catalog, booktag) 
+    return book_tag_size(catalog) 
 
 
-def first_book(catalog):
-    """
-    Devuelve el primer libro del catalogo
-    """
-    # TODO: Mods Est-3 en el Lab 2
-    pass
-
-
-def last_book(catalog):
-    """
-    Devuelve el ultimo libro del catalogo
-    """
-    # TODO: Mods Est-3 en el Lab 2
-    pass
+def first_book(catalog): 
+    """ 
+    Devuelve el primer libro del catalogo 
+    """ 
+    # TODO: Mods Est-3 en el Lab 2 
+    return set.get_first_element(catalog["books"]) 
+ 
+def last_Book(catalog): 
+    """ 
+    Devuelve el último libro del catalogo 
+    """ 
+    # TODO: Mods Est-3 en el Lab 2 
+    return set.get_last_element(catalog["books"]) 
 
 # Funciones para la manipulacion de los datos
 
@@ -180,32 +175,23 @@ def create_book_tag_list(catalog):
 
 def add_book_tag(catalog, booktag):
     """
-    Esta funcion agrega un elemento a lista de booktags.
-
-    :param catalog: Catalogo de la aplicación
-    :type catalog: dict
-    :param booktag: Tag a agregar
-    :type booktag: dict
-
-    :returns: catalog
-    :rtype: dict
-    """
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+   Esta función agrega un elemento a lista de booktags. 
+    """ 
+    # TODO: Mods de Est-1, Est-2 y Est-3 en Lab 2 
+    set.add_element(catalog["book_tags"], booktag) 
+    return catalog 
 
 
 # Funciones de consulta
 
-def book_size(catalog):
-    # TODO: Mods de Est-3 en el Lab 2
-    pass
-
-
-def tag_size(catalog):
-    # TODO: Mods de Est-3 en el Lab 2
-    pass
-
-
-def book_tag_size(catalog):
-    # TODO: Mods de Est-3 en el Lab 2
-    pass
+def book_size(catalog): 
+    # TODO Mods de Est-3 en el Lab 2 
+    return set.size(catalog["books"]) 
+ 
+def tag_size(catalog): 
+    # TODO Mods de Est-3 en el Lab 2 
+    return set.size(catalog["tags"]) 
+ 
+def book_tag_size(catalog): 
+    # TODO Mods de Est-3 en el Lab 2 
+    return set.size(catalog["book_tags"]) 
--- App/view.py
+++ App/view.py
@@ -48,18 +48,13 @@ def new_logic():
     return control
 
 
-def print_menu():
-    """
-    Imprime el menú de opciones en consola para el usuario
-    """
-    print("Opciones:")
-    print("1- Cargar Libros")
-    print("2- Cargar Tags")
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
-    print("0- Salir")
-
+def print_menu(): 
+    print("Opciones:") 
+    print("1- Cargar Libros") 
+    print("2- Cargar Tags") 
+    # TODO: Mods de Est-1, Est-2 y Est-3 en Lab 2 
+    print("3- Cargar Book-Tags!!!...") 
+    print("0- Salir") 
 
 def load_books(app):
     """
@@ -148,9 +143,11 @@ def main():
             tags = load_tags(app)
             print("Total de tags cargados: " + str(tags) + "\n")
 
-        elif int(inputs[0]) == 3:
-            # TODO: Mods de Est-3 en el Lab 2
-            pass
+        elif int(inputs[0]) == 3: 
+        # TODO: Mods de Est-3 en el Lab 2 
+            print("Cargando información de Book-Tags...") 
+            booktags = load_books_tags(app) 
+            print("Total de Book-Tags cargados: " + str(booktags)) 
 
         elif int(inputs[0]) == 0:
             working = False
